7602c4b240562e1493be7dfe3c73bac9db5b2ea3
G-ORKY <gorky@gorky.cn>
2024-04-30 23:20:58 +0800

    Latest update

--- Proxyserevrconfiguration.sh
+++ Proxyserevrconfiguration.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+
+echo "Thanks for using this script. You need to use the ROOT account to run this script. Wait 2 seconds to continue..."
+sleep 2
+
+echo "--------------------------------------------------------------- "
 echo "Please type the path that you want to install and set up sing-box in:"
+echo "--------------------------------------------------------------- "
 read installationpath
 
+echo "--------------------------------------------------------------- "
 echo "Please type the domain used on this server as the proxy server name:"
+echo "--------------------------------------------------------------- "
 read servername
 
+echo "--------------------------------------------------------------- "
 echo "Please type the user you want use for installation:"
+echo "--------------------------------------------------------------- "
 read usr
 homepath="/home/$usr"
 
@@ -52,8 +62,11 @@ then
         wget -P $installationpath/Hysteria/site/ https://raw.githubusercontent.com/G-ORKY/Proxy-server-initiallizer/main/re.html
         sudo chmod +777 $installationpath/Hysteria/site/re.html
         #download the configuration file and the site to redirect to when hysteria authentication is failed
+        echo "fetched basic web components for hysteria config and fallback site" \n >> ~/Hysteria/installation.log
 
+        echo "--------------------------------------------------------------- "
         echo "Choose the path you want to put your log file in, or leave it empty to use the default path($logpath):"
+        echo "--------------------------------------------------------------- "
         read logpath
         if $logpath=="";
         then
@@ -65,7 +78,11 @@ then
             #set the log path in the configuration file
         fi
 
+        echo "sing-box log path has been set" \n >> ~/Hysteria/installation.log
+
+        echo "--------------------------------------------------------------- "
         echo "Choose username you want to use:"
+        echo "--------------------------------------------------------------- "
         read username
         sed -i s/!usrname/$username/g $installationpath/Hysteria/config/Hysteriaconfig.json
         #set the username in the configuration file
@@ -73,6 +90,8 @@ then
         echo "Enterthe password you want to use:"
         read password
         sed -i s/!usrpassword/$password/g $installationpath/Hysteria/config/Hysteriaconfig.json
+
+        echo "User information has been set" \n >> ~/Hysteria/installation.log
         #set the password in the configuration file
 
         wget -P /home/$usr -O -  https://get.acme.sh | sh
@@ -82,11 +101,12 @@ then
         /home/$usr/.acme.sh/acme.sh --upgrade --auto-upgrade
         #install and turn on the auto upgrade for acme.sh
 
+        echo "--------------------------------------------------------------- "
         echo "Choose the option you want to use to obtain the certificate :"
+        echo "Option 2 will replace the privious nginx.conf file, so if you have any custom configuration, please choose option 1 or make sure that you have the backup of the nginx.conf and you need to re-add your privious config after configuration!!!"
+        echo "--------------------------------------------------------------- "
         echo "1."I have the site privious run on this server!""
         echo "2."I have the domain but it is not related to any site on this server and I AGREE to use the default site!""
-        echo " "
-        echo "Option 2 will replace the privious nginx.conf file, so if you have any custom configuration, please choose option 1 or make sure that you have the backup of the nginx.conf and you need to re-add your privious config after configuration!!!"
         
         read siteoption
         if $siteoption=="1"
@@ -110,7 +130,9 @@ then
         testoutcome=$(cat $installationpath/Hysteria/installation.log | grep 'error')
         if $testoutcome=="error";
         then
+            echo "--------------------------------------------------------------- "
             echo "Failed to obtain the certificate, please check the log file for more details."
+            echo "--------------------------------------------------------------- "
         else
             /home/$usr/.acme.sh/acme.sh --set-default-ca --server letsencrypt
             /home/$usr/.acme.sh/acme.sh --issue -d $servername -w $sitepath --keylength ec-256 --force
@@ -126,7 +148,9 @@ then
 
             sudo chmod +x $certpath/certrenew.sh
 
+            echo "--------------------------------------------------------------- "
             echo "Congratulations! All done! Please enter your password to start the sing-box. Feel free to use your proxy server!"
+            echo "--------------------------------------------------------------- "
             echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
             echo "Please remember to add the crontab to renew the certificate automatically, you can use the following command to add the crontab:"
             echo "# 1:00am, 1st day each month, run `certrenew.sh`"
@@ -136,10 +160,14 @@ then
         fi
 
     else
+        echo "--------------------------------------------------------------- "
         echo "This script is currently not supported on your OS, please contact us to request support for your OS."
+        echo "--------------------------------------------------------------- "
     fi
 else
+    echo "--------------------------------------------------------------- "
     echo "please use the root account to run this script."
     echo "btw you can use "sudo -i" and then run this script to set up."
+    echo "--------------------------------------------------------------- "
 fi
 
--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 PLEASE!!PLEASE!! Remenber to BACKUP your original nginx.conf if you have the demand!
 
 Or you can choose the option that you do not need to use the auto page set-up function to use your original page to obtain a cert for hysteria.
+
+Btw, this script is temporary for single-user setup.
+
+For more functions you wish to add to this script, feel free to contribute to this project or contact our contributers.
